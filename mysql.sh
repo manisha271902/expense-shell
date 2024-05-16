04d083fa4593713570d899d544f139d8025f4be9
manisha271902 <manishachennoju97@gmail.com>
2024-05-16 11:57:28 +0530

    expense shell

--- mysql.sh
+++ mysql.sh
@@ -38,19 +38,20 @@ systemctl start mysqld  &>>$LOG_FILE
 VALIDATE $? "starting mysql" 
 
 
-mysql_secure_installation --set-root-pass cherry123 &>>$LOG_FILE
-VALIDATE $? "Setting up root usr password" 
+# mysql_secure_installation --set-root-pass cherry123 &>>$LOG_FILE   
+# VALIDATE $? "Setting up root usr password" 
 
 
-# #Belowe code is used for idempotency  nature
-# mysql -h -uroot -p${db_pswd} -e "show databases;" &>>$LOG_FILE
-# if [ $? -ne 0 ]
-# then
-#     mysql_secure_installation --set-root-pass db_pswd &>>$LOG_FILE
-# else
-#     echo -e "mysql root password is already setup, so $Y skipping $N"
-# fi
+#Belowe code is used for idempotency  nature
+mysql -h db.manisha.fun -uroot -p${db_pswd} -e "show databases;" &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass db_pswd &>>$LOG_FILE
+else
+    echo -e "mysql root password is already setup, so $Y skipping $N"
+fi
 
     
 
 #ikada password dynamic ga istunam ->  mysql_secure_installation -uroot cherry123 -e "show databases;" &>>$LOG_FILE
+#mysql_secure_installation --set-root-pass cherry123 &>>$LOG_FILE  (ee cmd first time password set chesetapud matrame work avthadi)  
