f2da9163b1298be7dc01e7cd2492637f509cdfb3
manisha271902 <manishachennoju97@gmail.com>
2024-05-17 23:54:06 +0530

    expense shell

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
+if [ $USERID -ne 0 ]
+then
+    echo " $R please run with super user access $N"
+else
+    echo "$G You are a super user $N"
+fi
 
-
-
-
-
-
-
-
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 ....$R FAILED $N"
+    else
+        echo "$2..... $G SUCCESS $N"
+}
 
 
 dnf install nginx -y &>>$LOGFILE
