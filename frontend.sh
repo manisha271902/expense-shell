46cff72a6b20fa4bcb98453bbd8fa8034ee2038d
manisha271902 <manishachennoju97@gmail.com>
2024-05-17 23:56:39 +0530

    expense shell

--- frontend.sh
+++ frontend.sh
@@ -10,17 +10,17 @@ Y="\e[33m"
 
 if [ $USERID -ne 0 ]
 then
-    echo " $R please run with super user access $N"
+    echo -e " $R please run with super user access $N"
 else
-    echo "$G You are a super user $N"
+    echo -e "$G You are a super user $N"
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 ....$R FAILED $N"
+        echo -e  "$2 ....$R FAILED $N"
     else
-        echo "$2..... $G SUCCESS $N"
+        echo -e "$2..... $G SUCCESS $N"
     fi
 }
 
